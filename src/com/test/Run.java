4ade56cec7069c98c95f20a81f12d03d9ecb47c9
ShriVenom <shri1307sp@gmail.com>
2021-12-31 14:15:18 +0530

    Folder Structure

---
+++ src/com/test/Run.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import java.util.*;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> l=new ArrayList<String>();
+		l.add(1);
+		l.add("2");
+		for (String i : l) {
+			System.out.println(i);
+			
+		}
+		
+	
+	}
+}
+	
